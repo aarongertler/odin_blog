01c45f0654f812823873ea099cc1769880603650
Aaron Gertler <aaronlgertler@gmail.com>
2017-03-16 19:23:36 -0700

    Added form-based workflow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
   def index
     @articles = Article.all  # An instance variable, since view and controller both need to use it
   end # Anything we want available in the view template should be an instance variable
@@ -7,6 +9,51 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id]) # grabs :id from the url
     # params returns a hash of all request parameters, we've chosen "id" from among them
+  end
+
+  def new
+    @article = Article.new # We actually need to make an article before we can reference it with our form
+  
+    # Just as "new" leads automatically to "create", "edit" leads automatically to "update"
+  end
+
+  def create
+    @article = Article.new(article_params) # Defined further in articles_helper
+    # Above is more efficient than the below
+    # @article = Article.new(
+    #   title: params[:article][:title]
+    #   body: params[:article][:body] # This didn't work until we added the :article param first
+    #   )
+    @article.save
+    flash.notice = "You published '#{@article.title}'!"
+
+    redirect_to article_path(@article)  # Sends us to the show page once we're done creating
+    # fail    # Adding this lets us see lots of details about the request, handy for testing
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+    # Once we've saved our form, it looks for the "update" method, since we hit "submit"
+    # on an object that already exists
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)  # "Update" method is similar to "new" -> we pass in a hash of data, and an object changes
+
+    flash.notice = "You successfully updated '#{@article.title}'!"
+        # We'll need to add thie to the view template before it works
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy 
+
+    flash.notice = "You successfully deleted '#{@article.title}'."
+
+    redirect_to articles_path
   end
 
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+    # Without require and permit, we don't have Strong Parameters and will run into trouble
+    # This way, we aren't blindly saving any parameter someone tries to give our site!
+  end
+
 end
